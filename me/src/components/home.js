f01baa575de0b5b6abd26889b994bfecf51e1f89
ENZOMOTIVE <91072069+ENZOMOTIVE@users.noreply.github.com>
2024-05-30 16:36:34 +0530

    home -initial-commit

--- me/src/components/home.js
+++ me/src/components/home.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './home.css';
 
+const image = require('./profile-pic.jpg');
+
 function Home() {
   return (
     <div className="home">
-      <h1>Welcome to My Portfolio</h1>
-      <p>Scroll down to explore more about me.</p>
+      <div className="profile-container">
+        <img className="profile-pic" src={image} alt="Profile" />
+        <div className="profile-details">
+          <h1>Aayushman Bhaba Padhy</h1>
+          <p>Scroll down to explore more about me.</p>
+        </div>
+      </div>
     </div>
   );
 }
