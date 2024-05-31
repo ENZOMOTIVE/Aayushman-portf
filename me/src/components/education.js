d69caa2fa798f15e46558126447f28d156f2884f
ENZOMOTIVE <91072069+ENZOMOTIVE@users.noreply.github.com>
2024-05-31 12:52:09 +0530

    styleninfo-Comit

--- me/src/components/education.js
+++ me/src/components/education.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import './education.css';
-import Logo from './nist-logo.jpg'; // Import your college logo file here
+import nistLogo from './nist-logo.jpg';
+import khalikoteLogo from './khalikote-logo.jpg';
+import svcsLogo from './svcs-logo.jpg';
+// Import your college logo file here
 
 function Education() {
   return (
     <div className="education">
       <h2>Education</h2>
+
+
+      {/*NIST*/}
       <div className="education-item">
         <div className="education-details">
           <div className="logo-container">
-            <img src={Logo} alt="College Logo" className="college-logo" />
+            <img src={nistLogo} alt="College Logo" className="college-logo" />
           </div>
           <div className="education-text">
             <h3>Bachelors in Computer Science Engineering</h3>
@@ -17,10 +23,12 @@ function Education() {
           </div>
         </div>
       </div>
+
+      {/*Khalikote*/}
       <div className="education-item">
         <div className="education-details">
           <div className="logo-container">
-            <img src={Logo} alt="College Logo" className="college-logo" />
+            <img src={khalikoteLogo} alt="College Logo" className="college-logo" />
           </div>
           <div className="education-text">
             <h3>Higher Secondary Education</h3>
@@ -28,6 +36,21 @@ function Education() {
           </div>
         </div>
       </div>
+
+      {/*SVCS*/}
+      <div className="education-item">
+        <div className="education-details">
+          <div className="logo-container">
+            <img src={svcsLogo} alt="College Logo" className="college-logo" />
+          </div>
+          <div className="education-text">
+            <h3>Primary Education</h3>
+            <p>St. Vincent's Convent School Berhampur, Odisha, India (2019)</p>
+          </div>
+        </div>
+      </div>
+
+
     </div>
   );
 }
