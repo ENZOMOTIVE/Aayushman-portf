62ba083bbae3366ad609bc96e6030c0db7fca00c
ENZOMOTIVE <91072069+ENZOMOTIVE@users.noreply.github.com>
2024-05-31 15:12:58 +0530

    initial-commit

--- me/src/components/education.js
+++ me/src/components/education.js
@@ -11,19 +11,23 @@ function Education() {
 
       <div className="education-container">
         <div className="education-item right">
-          <div className="logo-container">
-            <img src={nistLogo} alt="College Logo" className="college-logo" />
-          </div>
+          <a href="https://www.nist.edu/" target="_blank" rel="noopener noreferrer" className="logo-link">
+            <div className="logo-container">
+              <img src={nistLogo} alt="NIST Logo" className="college-logo" />
+            </div>
+          </a>
           <div className="education-details">
             <h3>Bachelors in Computer Science Engineering</h3>
-            <p>NIST University, Berhampur Odisha, India (2021-2021) </p>
+            <p>NIST University, Berhampur Odisha, India (2021-2021)</p>
           </div>
         </div>
 
         <div className="education-item left">
-          <div className="logo-container">
-            <img src={khalikoteLogo} alt="College Logo" className="college-logo" />
-          </div>
+          <a href="https://kuu.ac.in/" target="_blank" rel="noopener noreferrer" className="logo-link">
+            <div className="logo-container">
+              <img src={khalikoteLogo} alt="Khalikote Logo" className="college-logo" />
+            </div>
+          </a>
           <div className="education-details">
             <h3>Higher Secondary Education</h3>
             <p>Khalikote Higher Secondary School, Berhampur, Odisha, India (2019 - 2021)</p>
@@ -31,9 +35,11 @@ function Education() {
         </div>
 
         <div className="education-item right">
-          <div className="logo-container">
-            <img src={svcsLogo} alt="College Logo" className="college-logo" />
-          </div>
+          <a href="https://stvcberhampur.com/" target="_blank" rel="noopener noreferrer" className="logo-link">
+            <div className="logo-container">
+              <img src={svcsLogo} alt="SVCS Logo" className="college-logo" />
+            </div>
+          </a>
           <div className="education-details">
             <h3>Primary Education</h3>
             <p>St. Vincent's Convent School Berhampur, Odisha, India (2019)</p>
