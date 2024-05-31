dd6b86776b824458429275d90543cf05a932baf7
ENZOMOTIVE <91072069+ENZOMOTIVE@users.noreply.github.com>
2024-05-31 13:24:33 +0530

    initial-commit

--- me/src/components/education.js
+++ me/src/components/education.js
@@ -3,54 +3,43 @@ import './education.css';
 import nistLogo from './nist-logo.jpg';
 import khalikoteLogo from './khalikote-logo.jpg';
 import svcsLogo from './svcs-logo.jpg';
-// Import your college logo file here
 
 function Education() {
   return (
     <div className="education">
       <h2>Education</h2>
 
-
-      {/*NIST*/}
-      <div className="education-item">
-        <div className="education-details">
+      <div className="education-container">
+        <div className="education-item right">
           <div className="logo-container">
             <img src={nistLogo} alt="College Logo" className="college-logo" />
           </div>
-          <div className="education-text">
+          <div className="education-details">
             <h3>Bachelors in Computer Science Engineering</h3>
             <p>NIST University, Berhampur Odisha, India (2021-2021) </p>
           </div>
         </div>
-      </div>
 
-      {/*Khalikote*/}
-      <div className="education-item">
-        <div className="education-details">
+        <div className="education-item left">
           <div className="logo-container">
             <img src={khalikoteLogo} alt="College Logo" className="college-logo" />
           </div>
-          <div className="education-text">
+          <div className="education-details">
             <h3>Higher Secondary Education</h3>
             <p>Khalikote Higher Secondary School, Berhampur, Odisha, India (2019 - 2021)</p>
           </div>
         </div>
-      </div>
 
-      {/*SVCS*/}
-      <div className="education-item">
-        <div className="education-details">
+        <div className="education-item right">
           <div className="logo-container">
             <img src={svcsLogo} alt="College Logo" className="college-logo" />
           </div>
-          <div className="education-text">
+          <div className="education-details">
             <h3>Primary Education</h3>
             <p>St. Vincent's Convent School Berhampur, Odisha, India (2019)</p>
           </div>
         </div>
       </div>
-
-
     </div>
   );
 }
