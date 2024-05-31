4770d393b83dd9ee04474e2c4a97de2e80ef3c50
ENZOMOTIVE <91072069+ENZOMOTIVE@users.noreply.github.com>
2024-05-31 15:45:43 +0530

    Work-commit

--- me/src/App.js
+++ me/src/App.js
@@ -3,6 +3,7 @@ import { Link, Element} from 'react-scroll';
 import './App.css';
 import Home from './components/home';
 import Education from './components/education';
+import WorkExperience from './components/workexperience'; 
 import Skills from './components/skills';
 import Projects from './components/projects';
 import Research from './components/research';
@@ -17,6 +18,7 @@ function App() {
           <ul>
             <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
             <li><Link to="education" smooth={true} duration={500}>Education</Link></li>
+            <li><Link to="work-experience" smooth={true} duration={500}>Work Experience</Link></li> 
             <li><Link to="skills" smooth={true} duration={500}>Skills</Link></li>
             <li><Link to="projects" smooth={true} duration={500}>Projects</Link></li>
             <li><Link to="research" smooth={true} duration={500}>Research</Link></li>
@@ -28,6 +30,7 @@ function App() {
 
       <Element name="home"><Home /></Element>
       <Element name="education"><Education /></Element>
+      <Element name="work-experience"><WorkExperience /></Element>
       <Element name="skills"><Skills /></Element>
       <Element name="projects"><Projects /></Element>
       <Element name="research"><Research /></Element>
---
+++ me/src/components/workexperience.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import './workexperience.css';
+import bariflo from './bariflo-logo.jpg';
+import pmn from './pmn-logo.jpg';
+import linkedinLogo from './linkdln-logo.png'; // Add your LinkedIn logo here
+
+function WorkExperience() {
+  return (
+    <div className="work-experience">
+      <h2>Work Experience</h2>
+
+      <div className="work-experience-container">
+        {/* Bariflo */}
+        <div className="work-experience-item right">
+          <div className="logo-container">
+            <img src={bariflo} alt="Company Logo" className="company-logo" />
+          </div>
+          <div className="work-experience-details">
+            <h3>Blockchain Developer Intern</h3>
+            <p>Bariflo Cybernetics, Bhubaneswar, India (15 May - 15 July 2024)</p>
+          </div>
+          <a href="https://www.linkedin.com/company/bariflo-labs" target="_blank" rel="noopener noreferrer" className="linkedin-logo-container">
+            <img src={linkedinLogo} alt="LinkedIn Logo" className="linkedin-logo" />
+          </a>
+        </div>
+
+        {/* PMN */}
+        <div className="work-experience-item left">
+          <div className="logo-container">
+            <img src={pmn} alt="Company Logo" className="company-logo" />
+          </div>
+          <div className="work-experience-details">
+            <h3>Web Developer Intern</h3>
+            <p>Work from Home (Dec - March 2024)</p>
+          </div>
+          <a href="https://www.linkedin.com/company/pmnpatralok" target="_blank" rel="noopener noreferrer" className="linkedin-logo-container">
+            <img src={linkedinLogo} alt="LinkedIn Logo" className="linkedin-logo" />
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default WorkExperience;
