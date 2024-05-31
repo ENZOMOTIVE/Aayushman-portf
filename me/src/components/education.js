31bb8036bf555c31ea0ae74844a8a13fb0a68c1c
ENZOMOTIVE <91072069+ENZOMOTIVE@users.noreply.github.com>
2024-05-31 12:35:35 +0530

    small commit

--- me/src/components/education.js
+++ me/src/components/education.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import './education.css';
+import Logo from './nist-logo.jpg'; // Import your college logo file here
 
 function Education() {
   return (
     <div className="education">
       <h2>Education</h2>
       <div className="education-item">
-        <h3>Bachelors in Computer Science Engineering</h3>
-        <p>NIST University, Berhampur Odisha, India  (2021-2021) </p>
+        <div className="education-details">
+          <div className="logo-container">
+            <img src={Logo} alt="College Logo" className="college-logo" />
+          </div>
+          <div className="education-text">
+            <h3>Bachelors in Computer Science Engineering</h3>
+            <p>NIST University, Berhampur Odisha, India (2021-2021) </p>
+          </div>
+        </div>
       </div>
       <div className="education-item">
-        <h3>Higher Secondary Education</h3>
-        <p>Khalikote Higher Secondary School, Berhampur, Odisha, India (2019 - 2021)</p>
+        <div className="education-details">
+          <div className="logo-container">
+            <img src={Logo} alt="College Logo" className="college-logo" />
+          </div>
+          <div className="education-text">
+            <h3>Higher Secondary Education</h3>
+            <p>Khalikote Higher Secondary School, Berhampur, Odisha, India (2019 - 2021)</p>
+          </div>
+        </div>
       </div>
     </div>
   );
