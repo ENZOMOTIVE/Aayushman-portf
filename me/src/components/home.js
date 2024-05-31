e78ab01c21f2750450370d0657a60a8fb810d5ce
ENZOMOTIVE <91072069+ENZOMOTIVE@users.noreply.github.com>
2024-05-31 12:12:27 +0530

    style-commit

--- me/src/components/home.js
+++ me/src/components/home.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './home.css';
 
 const image = require('./profile-pic.jpg');
 
 function Home() {
+  useEffect(() => {
+    const handleScroll = () => {
+      const nameElement = document.querySelector('.profile-details h1');
+      nameElement.classList.remove('typing');
+      // Trigger a reflow
+      void nameElement.offsetWidth;
+      nameElement.classList.add('typing');
+    };
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <div className="home">
       <div className="profile-container">
         <img className="profile-pic" src={image} alt="Profile" />
         <div className="profile-details">
-          <h1>Aayushman Bhaba Padhy</h1>
+          <h1 className="typing">Aayushman Bhaba Padhy</h1>
           <div className="about-box">
             <h2>About me</h2>
             <p>I am a B.TECH CSE final year Undergrad with growing Excitement in Blockchain and Cryptography</p>
