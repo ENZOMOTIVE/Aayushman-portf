1bea86389590ea7ff42b62740683dfd842725d9c
ENZOMOTIVE <91072069+ENZOMOTIVE@users.noreply.github.com>
2024-05-31 13:08:21 +0530

    scroll-commit

--- me/src/components/home.js
+++ me/src/components/home.js
@@ -1,9 +1,12 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import './home.css';
+import { Link } from 'react-scroll';
 
 const image = require('./profile-pic.jpg');
 
 function Home() {
+  const educationRef = useRef(null); // Create ref for the education section
+
   useEffect(() => {
     const handleScroll = () => {
       const nameElement = document.querySelector('.profile-details h1');
@@ -26,14 +29,18 @@ function Home() {
         <img className="profile-pic" src={image} alt="Profile" />
         <div className="profile-details">
           <h1 className="typing">Aayushman Bhaba Padhy</h1>
-          <div className="about-box">
+          <div className="about-box" ref={educationRef}>
             <h2>About me</h2>
             <p>I am a B.TECH CSE final year Undergrad with growing Excitement in Blockchain and Cryptography</p>
             <p>Full Stack Blockchain Developer</p>
           </div>
         </div>
       </div>
-      <div className="scroll-down">Scroll down to see more</div>
+      <Link to="education" smooth={true} duration={500}>
+        <div className="scroll-down">
+          Scroll down to see more
+        </div>
+      </Link>
     </div>
   );
 }
