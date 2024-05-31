5f04beb80eff79458c14596e251f4d56f1130f60
ENZOMOTIVE <91072069+ENZOMOTIVE@users.noreply.github.com>
2024-05-31 17:45:04 +0530

    style commit

--- me/src/components/skills.js
+++ me/src/components/skills.js
@@ -4,11 +4,19 @@ import jsLogo from './assets/javascript-logo.png';
 import reactLogo from './assets/reactJs-logo.png';
 import nodejsLogo from './assets/nodeJs-logo.png';
 import pythonLogo from './assets/Python-logo.jpg';
-// import mlLogo from './ml-logo.png';
-// import dataAnalysisLogo from './data-analysis-logo.png';
-// import blockchainLogo from './blockchain-logo.png';
 import solidityLogo from './assets/solidity-logo.png';
 import etherJsLogo from './assets/ether.js-logo.png';
+import metamaskLogo from './assets/metamask-logo.png';
+import GanacheLogo from './assets/ganache-logo.png';
+import TruffleLogo from './assets/truffle-logo.png';
+import HardhatLogo from './assets/hardhat-logo.jpg';
+import RemixLogo from './assets/remix-logo.png';
+import HtmlLogo from './assets/html-logo.png';
+import CssLogo from './assets/css-logo.png';
+import ExpressLogo from './assets/Express.js-logo.png';
+import IPFS from './assets/IPFS-logo.png';
+import Docker from './assets/docker-logo.png';
+// Import additional logos as needed
 
 function Skills() {
   return (
@@ -17,37 +25,90 @@ function Skills() {
       
       <div className="skills-section">
         <h3>Blockchain</h3>
+
         <div className="skills-items">
           <div className="skill-item">
             <img src={solidityLogo} alt="Solidity" className="skill-logo" />
           </div>
+          
           <div className="skill-item">
-            <img src={etherJsLogo} alt="Ether.js" className="skill-logo" />
+          <img src={etherJsLogo} alt="Ether.js" className="skill-logo"  />
+
           </div>
+          <div className="skill-item">
+            <img src={metamaskLogo} alt="Metamask" className="skill-logo" style={{ width: '100%', height: '90%', objectFit: 'cover' }} />
+          </div>
+
+          <div className="skill-item">
+            <img src={GanacheLogo} alt="Ganache" className="skill-logo"  />
+          </div>
+
+          <div className="skill-item">
+            <img src={TruffleLogo} alt="Truffle" className="skill-logo"  />
+          </div>
+
+          <div className="skill-item">
+            <img src={HardhatLogo} alt="Hardhat" className="skill-logo"  />
+          </div>
+
+          <div className="skill-item">
+            <img src={RemixLogo} alt="RemixIDE" className="skill-logo"  />
+          </div>
+
+          <div className="skill-item">
+            <img src={IPFS} alt="IPFS" className="skill-logo"  />
+          </div>
+          {/* Add more blockchain skill items here */}
         </div>
       </div>
 
       <div className="skills-section">
         <h3>Front End</h3>
         <div className="skills-items">
-          <div className="skill-item">
-            <img src={jsLogo} alt="JavaScript" className="skill-logo" />
+        <div className="skill-item">
+            <img src={ HtmlLogo} alt="HTML" className="skill-logo" style={{ width: '100%', height: '100%', objectFit: 'cover' }} />
           </div>
+
+          <div className="skill-item">
+            <img src={CssLogo} alt="CSS" className="skill-logo" style={{ width: '70%', height: '100%', objectFit: 'cover' }} />
+          </div>
+
+          
           <div className="skill-item">
             <img src={reactLogo} alt="React" className="skill-logo" />
           </div>
+
+
+          {/* Add more front end skill items here */}
         </div>
       </div>
 
       <div className="skills-section">
         <h3>Back End</h3>
         <div className="skills-items">
+        <div className="skill-item">
+            <img src={jsLogo} alt="JavaScript" className="skill-logo" />
+          </div>
+
           <div className="skill-item">
+            
             <img src={nodejsLogo} alt="Node.js" className="skill-logo" />
           </div>
+
+          <div className="skill-item" style={{ width: '89%', height: '69%', objectFit: 'cover' }} >
+            <img src={ExpressLogo} alt="Express" className="skill-logo" />
+          </div>
+
           <div className="skill-item">
             <img src={pythonLogo} alt="Python" className="skill-logo" />
           </div>
+
+          <div className="skill-item">
+            <img src={Docker} alt="Docker" className="skill-logo" />
+          </div>
+
+
+          {/* Add more back end skill items here */}
         </div>
       </div>
     </div>
