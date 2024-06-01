23de34a9739618d9ce02d762310f64e577ce3b8d
ENZOMOTIVE <91072069+ENZOMOTIVE@users.noreply.github.com>
2024-06-01 13:18:50 +0530

    final-commit

--- me/src/components/contact.js
+++ me/src/components/contact.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import './contact.css';
+import linkedinIcon from './linkdln-logo.png';
+import githubIcon from './assets/github-logo.jpeg';
+import emailIcon from './assets/gmail.png';
 
 function Contact() {
   return (
     <div className="contact">
       <h2>Contact Me</h2>
-      <p>You can reach me via the following platforms:</p>
+      
       <div className="contact-links">
-        <a href="https://linkedin.com/in/yourprofile" target="_blank" rel="noopener noreferrer">
-          <img src="linkedin-icon.png" alt="LinkedIn" />
+        <a href="mailto:aayushmanbhabapadhy@gmail.com">
+          <img src={emailIcon} alt="Email" />
         </a>
-        <a href="https://github.com/yourprofile" target="_blank" rel="noopener noreferrer">
-          <img src="github-icon.png" alt="GitHub" />
+        <a href="https://www.linkedin.com/in/aayushman-bhaba-padhy/" target="_blank" rel="noopener noreferrer">
+          <img src={linkedinIcon} alt="LinkedIn" />
+        </a>
+        <a href="https://github.com/ENZOMOTIVE" target="_blank" rel="noopener noreferrer">
+          <img src={githubIcon} alt="GitHub" />
         </a>
       </div>
     </div>
